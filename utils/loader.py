97c8f9f5a8f3a4f63dbfadfd6b5da27bd91db212
wwwbbb8510 <wwwbbb8510@163.com>
2020-01-25 10:45:57 +1300

    change the image size of training imagenet

--- utils/loader.py
+++ utils/loader.py
@@ -191,6 +191,8 @@ def torch_vision_load_imagenet(is_aug, download=False):
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         ])
     test_transform_imagenet = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ])
